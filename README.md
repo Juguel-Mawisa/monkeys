0ecce2c2377417f2fea6286747ebaecc7f05aa0d
juguel mawisa <juguelmawisa@gmail.com>
2026-06-06 06:04:50 +0100

    feat: initialize Next.js project with TypeScript, Tailwind CSS, and SEO component
    - Add package.json with dependencies for Next.js, React, and TypeScript
    - Create postcss.config.js for Tailwind CSS and autoprefixer
    - Implement custom _app.tsx and _document.tsx for global styles and document structure
    - Add index.tsx as the homepage with a simple greeting and SEO component
    - Create globals.css to include Tailwind CSS styles
    - Develop Seo component for managing page metadata
    - Configure Tailwind CSS with tailwind.config.js
    - Set up TypeScript configuration in tsconfig.json

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+## coders monkeys
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Seo from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Monkeys Coders" description="Welcome to the Monkeys Coders website!" />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
